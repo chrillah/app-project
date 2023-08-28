4e1b7b086974810b00119687c5a6a850caa3eb2f
chrillah <ch.ronnberg@gmail.com>
2023-08-28 13:53:11 +0200

    add

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,17 @@
+// Short
+export class Ingredient{
+    constructor(public name: string, public amount:number){
+    }
+}
+
+
+// LONG
+// export class Ingredient{
+//     public name: string;
+//     public amount: number;
+
+//     constructor(name: string, amount:number){
+//         this.name = name;
+//         this.amount = amount
+//     }
+// }
